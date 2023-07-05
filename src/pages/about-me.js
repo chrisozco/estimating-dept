f5f2d48e1fe0ebe1218772cf0197a0265a53cd00
Chris Orozco <chrisorozco15@yahoo.com>
2023-07-05 10:17:02 -0700

    about me copy update

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -21,7 +21,7 @@ export default function AboutMe() {
                         <h1 id={styles.title}>Who is this Guy?</h1>
                     </div>
                     <div className={styles.sectionOne}>
-                        <p className={styles.sectionOneText}>Mark grew up in a North Seattle neighborhood. He spent most of his early childhood and teen years in a craftsman style house built in 1907. Growing up in an old house with lots of character sparked an early interest in how things are built. All through middle and high school he worked for a general contractor, who was a family friend, doing residential remodels around the North Seattle area.</p>
+                        <p className={styles.sectionOneText}>I grew up in a North Seattle neighborhood. I spent most of my early childhood and teen years in a craftsman style house built in 1907. Growing up in an old house with lots of character sparked an early interest in how things are built. All through middle and high school I worked for a general contractor, who was a family friend, doing residential remodels around the North Seattle area.</p>
                         <Image 
                         className={styles.aboutMeImgOne}
                         src='/images/sexauer_portrait.jpg'
@@ -39,12 +39,12 @@ export default function AboutMe() {
                         alt='Mark Portrait Image'
                         />
                         <div className={styles.sectionTwoText}>
-                            <p>After a tour in Iraq in 2003/2004 and a stint in the bar/restaurant industry Mark embraced his early passion for working in construction.</p>
-                            <p>Growing frustrated with contentious contractor vs homeowner relationships Mark decided to help spread awareness to improve these conditions. Content is intended for anyone interested in construction. From first-time homeowners to seasoned contractors and even developers.</p>
+                            <p>After a tour in Iraq in 2003/2004 and a stint in the bar/restaurant industry I embraced my early passion for working in construction.</p>
+                            <p>Growing frustrated with contentious contractor vs homeowner relationships I decided to help spread awareness to improve these conditions. Content is intended for anyone interested in construction. From first-time homeowners to seasoned contractors and even developers.</p>
                         </div>
                     </div>
                     <div className={styles.sectionThree}>
-                        <p>When not at work I loves spending time in my craftsman style house in West Seattle, gardening, baking bread and cooking for my wife and kids. Oh and skiing during the season!</p>
+                        <p>When not at work I love spending time in my craftsman style house in West Seattle, gardening, baking bread and cooking for my wife and kids. Oh and skiing during the season!</p>
                     </div>
                     <div className={styles.sectionFour}>
                         <Link href="/" className={styles.linkHome}>Back Home</Link>
