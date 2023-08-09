c1d4201b77472c6666d27c9a8e9b9cf6db265524
Chris Orozco <chrisorozco15@yahoo.com>
2023-08-08 21:03:45 -0700

    Framer Motion Add-ons

--- src/components/ContainerV2/Container.js
+++ src/components/ContainerV2/Container.js
@@ -2,12 +2,26 @@ import ClassName from 'models/classname';
 
 import styles from './Container.module.scss';
 
+import { motion, AnimatePresence } from 'framer-motion';
+
 const Container = ({ children, className }) => {
   const containerClassName = new ClassName(styles.container);
 
   containerClassName.addIf(className, className);
 
-  return <div className={containerClassName.toString()}>{children}</div>;
+  return (
+    <AnimatePresence>
+      <motion.div 
+      className={containerClassName.toString()}
+      initial={{ opacity: 0, y: 15}}
+      animate={{ opacity: 1, y: 0}}
+      exist={{ opacity: 0, y: 15}}
+      transition={{delay: 0.65}}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  )
 };
 
 export default Container;
